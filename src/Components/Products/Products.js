1f71ed015d23b4ebf1fb1efc17cfcb2dc0952525
Kawsar-Hossain-318 <thekawsarhossain@gmail.com>
2021-10-02 00:26:37 +0600

    dynamic rating added

--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import { Button } from 'react-bootstrap';
+import Rating from 'react-rating';
 
 const Products = (props) => {
-    const { img, name, price, seller, stock } = props.product;
+    const { img, name, price, seller, stock, star } = props.product;
     const { handleProductItem } = props
     return (
         <div className="row text-start border-bottom my-3 p-2">
@@ -16,7 +17,13 @@ const Products = (props) => {
                 <h6>Price: <mark>{price}</mark></h6>
                 <p>Seller: {seller}</p>
                 <p>Available: {stock}</p>
-                <Button className="border-0" style={{ backgroundColor: "#FF9800" }} onClick={() => handleProductItem(props.product)}><FontAwesomeIcon icon={faShoppingCart} /> Add Item</Button>
+                <Rating
+                    style={{ color: "#FF9800" }}
+                    emptySymbol="far fa-star"
+                    fullSymbol="fas fa-star"
+                    initialRating={star}
+                /> <br />
+                <Button className="border-0 mt-2" style={{ backgroundColor: "#FF9800" }} onClick={() => handleProductItem(props.product)}><FontAwesomeIcon icon={faShoppingCart} /> Add Item</Button>
             </div>
         </div>
     );
